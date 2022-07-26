81116882eb9def7713ba0819c1dd2ff44b4d0855
abdulhamiid <adioabdulhamid@yahoo.com>
2022-07-26 14:43:39 +0100

    import store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './components/redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+      ,
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
