322de21777b1b0a4abfdd933d0d7c6a2d3145573
abdulhamiid <adioabdulhamid@yahoo.com>
2022-07-26 15:44:47 +0100

    Add new routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/views/Home';
+import About from './components/views/About';
 import NotMatch from './components/views/NotMatch';
 import Nav from './components/views/Nav';
 import Africa from './components/continents/Africa';
 import Asia from './components/continents/Asia';
 import Europe from './components/continents/Europe';
+import NorthAmerica from './components/continents/NorthAmerica';
+import SouthAmerica from './components/continents/SouthAmerica';
+import AustraliaOcenia from './components/continents/AustraliaOceania';
 
 function App() {
   return (
@@ -13,11 +17,13 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="/africa" element={<Africa />} />
         <Route path="/asia" element={<Asia />} />
         <Route path="/europe" element={<Europe />} />
-        {/* <Route path="nestedOne" element={<NestedAbout />} />
-          <Route path="nestedTwo" element={<NestedAbout2 />} /> */}
+        <Route path="/north-america" element={<NorthAmerica />} />
+        <Route path="/south-america" element={<SouthAmerica />} />
+        <Route path="/australia-oceania" element={<AustraliaOcenia />} />
         <Route path="*" element={<NotMatch />} />
       </Routes>
     </>
