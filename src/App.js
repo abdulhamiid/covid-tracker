c7a16c11e06b73b7896e9d86755fd585ceb9f18c
abdulhamiid <adioabdulhamid@yahoo.com>
2022-07-26 14:43:25 +0100

    Add page routes to App.js file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/views/Home';
+import NotMatch from './components/views/NotMatch';
+import Nav from './components/views/Nav';
+import Africa from './components/continents/Africa';
+import Asia from './components/continents/Asia';
+import Europe from './components/continents/Europe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/africa" element={<Africa />} />
+        <Route path="/asia" element={<Asia />} />
+        <Route path="/europe" element={<Europe />} />
+        {/* <Route path="nestedOne" element={<NestedAbout />} />
+          <Route path="nestedTwo" element={<NestedAbout2 />} /> */}
+        <Route path="*" element={<NotMatch />} />
+      </Routes>
+    </>
+
   );
 }
 
