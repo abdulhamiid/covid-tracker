db55f21fcb0ab0f1b1acf4ea11047a685104ec98
abdulhamiid <adioabdulhamid@yahoo.com>
2022-07-27 16:41:41 +0100

    Add pagination

--- src/components/continents/FetchApi.js
+++ src/components/continents/FetchApi.js
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchData } from '../redux/api';
+import { fetchCountry } from '../redux/fetchCases';
+import Pagination from '../views/Pagination';
 
 function FetchApi({ continent }) {
   const result = useSelector((state) => state.result);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemPerPage] = useState(6);
   const dispatch = useDispatch();
   dispatch(fetchData(continent));
+
+  const handleClick = (e) => {
+    const { id } = e.target;
+    dispatch(fetchCountry(continent, id));
+  };
+
+  const indexOfLastItem = currentPage * itemPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemPerPage;
+  const currentResult = result.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div>
       <h1>{continent}</h1>
       <ul>
-        {result.map((item) => (
+        {currentResult.map((item) => (
           <li key={item.country}>
-            <Link to={`${item.country}`} id={result.indexOf(item)}>{item.country}</Link>
+            <Link to="/cases" id={result.indexOf(item)} onClick={handleClick} exact="true">{item.country}</Link>
           </li>
         ))}
       </ul>
+      <Pagination itemPerPage={itemPerPage} totalItem={result.length} paginate={paginate} />
     </div>
   );
 }
+
+FetchApi.propTypes = {
+  continent: PropTypes.string.isRequired,
+};
 
 export default FetchApi;
