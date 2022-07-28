7f561859d16197a26d9fc493f078bad989b87a02
abdulhamiid <adioabdulhamid@yahoo.com>
2022-07-28 22:21:03 +0100

    Create container layout for data fetched

--- src/components/views/Cases.js
+++ src/components/views/Cases.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { AiOutlineLeft } from 'react-icons/ai';
+import { IoMdSettings } from 'react-icons/io';
+import { RiMicFill } from 'react-icons/ri';
+import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 function Cases() {
@@ -6,38 +10,46 @@ function Cases() {
   const {
     flagURL, country, continent, population, cases, recovered, deaths,
   } = result;
+  const route = continent.replace(/\s/g, '-').toLowerCase();
 
   return (
-    <div>
-      <p>
-        Continent
-        {' - '}
-        {continent}
-      </p>
-      <h1>
-        {country}
-      </h1>
-      <img src={flagURL} alt="flag" />
-      <p>Country Information</p>
-      <ul>
-        <li>
-          Population
-          {population}
-        </li>
-        <li>
-          Cases
-          {cases}
-        </li>
-        <li>
-          Deaths
-          {deaths}
-        </li>
-        <li>
-          Recorvered
-          {recovered}
-        </li>
-      </ul>
-    </div>
+    <>
+      <nav className="navBar">
+        <ul>
+          <li>
+            <NavLink to={`/${route}`}>
+              <AiOutlineLeft />
+            </NavLink>
+          </li>
+          <li className="right-link">
+            <RiMicFill />
+            <NavLink to="/about">
+              <IoMdSettings />
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className="statistics">
+        <img src={flagURL} alt="flag" />
+        <h1>{country}</h1>
+        <div>
+          <ul>
+            <li>Continent</li>
+            <li>Population</li>
+            <li>Cases</li>
+            <li>Recorvered</li>
+            <li>Deaths</li>
+          </ul>
+          <ul>
+            <li>{continent}</li>
+            <li>{population}</li>
+            <li>{cases}</li>
+            <li>{recovered}</li>
+            <li>{deaths}</li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
