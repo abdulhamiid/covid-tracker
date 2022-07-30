db0ea7223b7e6f15cc06a2232e31a5d9c81ad788
abdulhamiid <adioabdulhamid@yahoo.com>
2022-07-30 07:27:03 +0100

    Remove countryCasesReducer from the redux store

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import countryCasesReducer from './fetchCases';
 import loadDataReducer from './loadApi';
 
 const store = configureStore({
   reducer: {
     data: loadDataReducer,
-    cases: countryCasesReducer,
   },
 });
 
