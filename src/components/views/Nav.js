2844a4d3cccdf8fd6e874a6106fd6d02ae6d297f
abdulhamiid <adioabdulhamid@yahoo.com>
2023-04-23 19:46:06 +0100

    Add useNavigate to go back to the previous page

--- src/components/views/Nav.js
+++ src/components/views/Nav.js
@@ -1,33 +1,28 @@
 import React from 'react';
 import { AiOutlineLeft } from 'react-icons/ai';
 import { IoIosSearch } from 'react-icons/io';
-
 import { MdMic } from 'react-icons/md';
-import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-const Nav = () => (
+const Nav = () => {
+  const navigate = useNavigate();
 
-  <nav className="navBar">
-    <ul>
-      <li>
-        <NavLink to="/">
-          <AiOutlineLeft />
-          Home
-        </NavLink>
-      </li>
-      <li className="right-link">
-        <MdMic />
-
-        <NavLink to="/about">
-          {/* <IoMdSettings /> */}
-        </NavLink>
-      </li>
-      <li>
-        <button type="button" onClick={() => { document.querySelector('.searchContainer').classList.toggle('hide'); }}>
-          <IoIosSearch />
-        </button>
-      </li>
-    </ul>
-  </nav>
-);
+  return (
+    <nav className="navBar">
+      <ul>
+        <li>
+          <AiOutlineLeft onClick={() => navigate(-1)} />
+        </li>
+        <li className="right-link">
+          <MdMic />
+        </li>
+        <li>
+          <button type="button" onClick={() => { document.querySelector('.searchContainer').classList.toggle('hide'); }}>
+            <IoIosSearch />
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 export default Nav;
